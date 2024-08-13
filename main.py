59264ad0d0686b7987118c649eb54922b983d440
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2024-08-13 11:56:24 +0530

    Save trained SVM model as siren_detector_model.pkl

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import librosa
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 import joblib
+
+# Fixed length for MFCC feature vectors
+FIXED_LENGTH = 1000
 
 # Load and preprocess audio data
 def load_audio(file_path):
     y, sr = librosa.load(file_path, sr=None)
     mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    # Pad or truncate MFCCs to fixed length
+    if mfccs.shape[1] < FIXED_LENGTH:
+        # Pad with zeros
+        mfccs = np.pad(mfccs, ((0, 0), (0, FIXED_LENGTH - mfccs.shape[1])), mode='constant')
+    else:
+        # Truncate to fixed length
+        mfccs = mfccs[:, :FIXED_LENGTH]
     return mfccs.flatten()
 
-# Example dataset
-audio_files = ['siren1.wav', 'siren2.wav', 'noise1.wav', 'noise2.wav']
+# Example dataset (MPEG files)
+audio_files = ['siren1.mpeg', 'siren2.mpeg', 'noise1.mpeg', 'noise2.mpeg']
 labels = [1, 1, 0, 0]  # 1 for siren, 0 for noise
 
 X = np.array([load_audio(file) for file in audio_files])
